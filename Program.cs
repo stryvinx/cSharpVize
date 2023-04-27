c006dac18494f7d9cc90870cec74bcfa766d9348
stryvinx <115171059+stryvinx@users.noreply.github.com>
2023-04-27 17:09:35 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿Category mainCategoryElektronik = new Category("Elektronik");
+Category subCategoryTelefon = new Category("Telefon");
+Category subCategoryBilgisayar = new Category("Bilgisayar");
+Category subCategoryTelevizyon = new Category("Televizyon");
+
+Category mainCategoryEv = new Category("Ev");
+Category subCategoryAydinlatma = new Category("Aydınlatma");
+Category subCategoryMobilya = new Category("Mobilya");
+
+
+
+
+mainCategoryElektronik.AddSubcategory(subCategoryTelefon);
+mainCategoryElektronik.AddSubcategory(subCategoryBilgisayar);
+mainCategoryElektronik.AddSubcategory(subCategoryTelevizyon);
+mainCategoryEv.AddSubcategory(subCategoryAydinlatma);
+mainCategoryEv.AddSubcategory(subCategoryMobilya);
+mainCategoryEv.AddSubcategory(subCategoryTelevizyon);
+
+Category.Print(mainCategoryElektronik);
+Category.Print(mainCategoryEv);
+
+public class Category
+{
+    public string Name { get; set; }
+    public List<Category> Subcategories { get; set; }
+
+    public Category(string name)
+    {
+        Name = name;
+        Subcategories = new List<Category>();
+    }
+
+    public void AddSubcategory(Category subcategory)
+    {
+        Subcategories.Add(subcategory);
+    }
+
+    public void RemoveSubcategory(Category subcategory)
+    {
+        Subcategories.Remove(subcategory);
+    }
+
+
+    public static void Print(Category category)
+    {
+        Console.WriteLine("Kategori: " + category.Name);
+        for (int i = 0; i < category.Subcategories.Count; i++)
+        {
+            Console.WriteLine("  Alt kategori: " + category.Subcategories[i].Name);
+            if (category.Subcategories[i].Subcategories.Count > 0)
+            {
+                Print(category.Subcategories[i]);
+            }
+        }
+    }
+}
+
